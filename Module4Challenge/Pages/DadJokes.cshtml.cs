1f143eff629b4c06768f64f67fcc3eef79f51573
AliviahRodeJones <ii3862au@go.minnstate.edu>
2026-09-18 22:56:39 +0000

    First debugged draft of Module 4 Challenge

--- Module4Challenge/Pages/DadJokes.cshtml.cs
+++ Module4Challenge/Pages/DadJokes.cshtml.cs
@@ -8,10 +8,10 @@ namespace Module4Challenge.Pages
     {
         public string[] dadJokes { get; set; } = new string[12]
         {
-            "When does a joke become a dad joke. When it becomes apparent.",
-            "What do you call a well-balanced horse? Stable.",
-            "I was wondering why the baseball was getting closer and closer. And then it hit me.",
-            "Where do polar bears keep their money? In a snowbank.",
+            "Joke1", //"When does a joke become a dad joke. When it becomes apparent.",
+            "Joke2", //"What do you call a well-balanced horse? Stable.",
+            "Joke3",//"I was wondering why the baseball was getting closer and closer. And then it hit me.",
+            "Joke4",//"Where do polar bears keep their money? In a snowbank.",
             "Joke5",
             "Joke6",
             "Joke7",
@@ -24,39 +24,101 @@ namespace Module4Challenge.Pages
 
         public int numberOfJokesToShow { get; set; } = 2;
         public string[] jokesToShow { get; set; } = new string[2];
+        public string[] newJokeList { get; set; } = new string[2];
+
 
 
     public void OnGet()
         {
              
             Random rnd = new Random();
+            // Creates a new random object. 
+            int randomJokeNumber = rnd.Next(0,12);
+            // Generates a new randomJokeNumber between 0 and 11. 
+
             for (int x = 0; x < numberOfJokesToShow; x++)
+            // Loops for numberOfJokesToShow. x represents the current joke in newJokeList.
             {
-                int currentJoke = rnd.Next(0,12);
-                while (string.IsNullOrEmpty(dadJokes[currentJoke]))
-                {
-                    currentJoke = rnd.Next(1,13);
+                
+                string currentJoke = dadJokes[randomJokeNumber]; 
+                // currentJoke is set to the value in dadJokes that the int randomJokeNumber is. This sets the r
+
+                for(int c = 0; c < jokesToShow.Length; c++){
+                    // Loops over jokesToShow. c represents the current joke in jokesToShow. 
+                    if(jokesToShow[c] == currentJoke)
+                    {
+                        randomJokeNumber = rnd.Next(0,12);
+                        currentJoke = dadJokes[randomJokeNumber];
+                        // Sets the randomJokeNumber to an new random value and sets the currentJoke to a new random string in dadJokes. 
+                        x = 0;
+                        // Sets x to 0 so that the jokesToShow list is looped over again.
+                        continue;
+                    }
+
+                
                 }
-                jokesToShow[x] = dadJokes[currentJoke];
-                dadJokes[currentJoke] = "";
+                
+                newJokeList[x] = currentJoke;
+                // The end of the x for-loop. After the c for-loop is done checking the joke is original, this line set the 
+                // x jokesToShow value as the currentJoke. 
+
+                //randomJokeNumber = rnd.Next(0,12);
+                // Sets randomJokeNumber to a new random number.
+                 
             }
+
+            Array.Copy(newJokeList, jokesToShow, newJokeList.Length);
+            // Copies the values in newJokesList to jokesToShow.
         }
 
     public void OnPost()
         {
-
+             
             Random rnd = new Random();
-            for (int x = 0; x < jokesToShow.Length; x++)
+            // Creates a new random object. 
+            int randomJokeNumber = rnd.Next(0,12);
+            // Generates a new randomJokeNumber between 0 and 11. 
+            
+            for (int x = 0; x < numberOfJokesToShow; x++)
+            // Loops the total number of times for the number of jokes to show. x is the current chosen joke in newJokeList.
             {
-                int currentJoke = rnd.Next(1,13);
-                while (string.IsNullOrEmpty(dadJokes[currentJoke]))
-                {
-                    currentJoke = rnd.Next(1,13);
+                
+                string currentJoke = dadJokes[randomJokeNumber]; 
+
+                for(int c = 0; c < jokesToShow.Length; c++){
+                    // Loops over jokesToShow. c represents the current joke in jokesToShow
+                    if(jokesToShow[c] == currentJoke)
+                    // If the current value in jokesToShow matches the currentJoke
+                    {
+                        randomJokeNumber = rnd.Next(0,12);
+                        x = 0;
+                        continue;
+                    }
+
+                
                 }
-                jokesToShow[x] = dadJokes[currentJoke];
-                dadJokes[currentJoke] = "";
+                
+                newJokeList[x] = currentJoke;
+                // The end of the x for-loop. After the c for-loop is done checking the joke is original, this line set the 
+                // x jokesToShow value as the currentJoke. 
+                randomJokeNumber = rnd.Next(0,12);
+                for (int y = 0; y < newJokeList.Length; y++)
+                {
+                    if(dadJokes[randomJokeNumber] == newJokeList[y])
+                    {
+                        randomJokeNumber = rnd.Next(0,12);
+                        y = 0;
+                        continue;
+                    }
+
+                }
+                // This block of code checks if the joke that matches with the new randomly generated number 
+                // matches a joke in the newJokeList. if so, a new random number is generated. 
+                 
             }
 
+            Array.Copy(newJokeList, jokesToShow, newJokeList.Length);
+            // Copies the values in newJokesList to jokesToShow.
         }
 
     }
--- Module4Challenge/obj/Debug/net10.0/Module4Challenge.AssemblyInfo.cs
+++ Module4Challenge/obj/Debug/net10.0/Module4Challenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module4Challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f43eadf2d642cd6ad63b978e7b336b0057f6a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb4770e49edada7ba523d25295bc82360c54316")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module4Challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module4Challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
